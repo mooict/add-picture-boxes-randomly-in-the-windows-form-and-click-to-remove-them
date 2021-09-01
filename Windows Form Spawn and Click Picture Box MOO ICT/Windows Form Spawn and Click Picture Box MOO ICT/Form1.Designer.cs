706e43bc840d463e96d427f6e683edb4d5411097
AZ <67914277+mooict@users.noreply.github.com>
2021-09-01 13:49:54 +0100

    projects files added

---
+++ Windows Form Spawn and Click Picture Box MOO ICT/Windows Form Spawn and Click Picture Box MOO ICT/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Windows_Form_Spawn_and_Click_Picture_Box_MOO_ICT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblItemCount = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblItemCount
+            // 
+            this.lblItemCount.AutoSize = true;
+            this.lblItemCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblItemCount.Location = new System.Drawing.Point(13, 13);
+            this.lblItemCount.Name = "lblItemCount";
+            this.lblItemCount.Size = new System.Drawing.Size(95, 25);
+            this.lblItemCount.TabIndex = 0;
+            this.lblItemCount.Text = "Items: 0";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 1000;
+            this.GameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblItemCount);
+            this.Name = "Form1";
+            this.Text = "Spawn and Click on Picture Box MOO ICT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblItemCount;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ Windows Form Spawn and Click Picture Box MOO ICT/Windows Form Spawn and Click Picture Box MOO ICT/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Form_Spawn_and_Click_Picture_Box_MOO_ICT
+{
+    public partial class Form1 : Form
+    {
+
+        Random rand = new Random();
+        List<PictureBox> items = new List<PictureBox>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void MakePictureBox()
+        {
+
+            PictureBox newPic = new PictureBox();
+            newPic.Height = 50;
+            newPic.Width = 50;
+            newPic.BackColor = Color.Maroon;
+
+            int x = rand.Next(10, this.ClientSize.Width - newPic.Width);
+            int y = rand.Next(10, this.ClientSize.Height - newPic.Height);
+            newPic.Location = new Point(x, y);
+
+            newPic.Click += NewPic_Click;
+
+            items.Add(newPic);
+            this.Controls.Add(newPic);
+        }
+
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            PictureBox temPic = sender as PictureBox;
+
+            items.Remove(temPic);
+
+            this.Controls.Remove(temPic);
+
+            lblItemCount.Text = "Items: " + items.Count();
+        }
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            MakePictureBox();
+
+            lblItemCount.Text = "Items: " + items.Count();
+        }
+    }
+}
